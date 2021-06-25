53b9f18ea32cd69db0598077322d34e72fdd5c30
Siddharth Singh <sidsgsits@gmail.com>
2021-06-26 01:33:55 +0530

    Files included

---
+++ todo.js
@@ -0,0 +1,80 @@
+function getAndUpdate(){
+    console.log("Updating List...");
+    tit = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+    if(tit.length==0){
+        if (confirm("Title is Empty, is it OK?")){
+            console.log('Ok for null entry!!');
+            if (localStorage.getItem('itemsJson')==null){
+                itemJsonArray = [];
+                itemJsonArray.push([tit, desc]);
+                localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+            }
+            else{
+                itemJsonArrayStr = localStorage.getItem('itemsJson')
+                itemJsonArray = JSON.parse(itemJsonArrayStr);
+                itemJsonArray.push([tit, desc]);
+                localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+            }
+            update();
+            }
+    }
+    else{
+        if (localStorage.getItem('itemsJson')==null){
+            itemJsonArray = [];
+            itemJsonArray.push([tit, desc]);
+            localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+        }
+        else{
+            itemJsonArrayStr = localStorage.getItem('itemsJson')
+            itemJsonArray = JSON.parse(itemJsonArrayStr);
+            itemJsonArray.push([tit, desc]);
+            localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+        }
+        update();
+    }
+}
+
+function update(){
+    if (localStorage.getItem('itemsJson')==null){
+        itemJsonArray = []; 
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    } 
+    else{
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr); 
+    }
+    // Populate the table
+    let tableBody = document.getElementById("tableBody");
+    let str = "";
+    itemJsonArray.forEach((element, index) => {
+        str += `
+        <tr>
+        <th scope="row">${index + 1}</th>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td> 
+        <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td> 
+        </tr>`; 
+    });
+    tableBody.innerHTML = str;
+}
+add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+update();
+function deleted(itemIndex){
+    console.log("Delete", itemIndex);
+    itemJsonArrayStr = localStorage.getItem('itemsJson')
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    // Delete itemIndex element from the array
+    itemJsonArray.splice(itemIndex, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    update();
+
+}
+function clearStorage(){
+    if (confirm("Do you really want to clear?")){
+    console.log('Clearing the storage')
+    localStorage.clear();
+    update()
+    }
+}
